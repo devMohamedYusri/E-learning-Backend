6ddda75f2b361e645eee5447e72c5cba40db4dd3
Mohamed Yusri <mohamedyousry373,d@gmail.com>
2024-10-16 16:11:39 +0300

    solve error file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db'); 
-const userRoutes = require('./routes/user.route');
+const userRoute = require('./routes/user.route');
 const coursesRoutes=require("./routes/course.route");
 const app = express();
 connectDB();
 app.use(express.json());
-app.use("/api/users",userRoutes);
+app.use("/api/users",userRoute);
 app.use('/api/courses',coursesRoutes);
 const PORT = process.env.PORT || 5000;
 
